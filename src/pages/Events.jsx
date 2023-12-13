2f194dd7b6eb1162fe5b864f36b852b055f6756e
Chibuike Ukandu <67115735+Jay035@users.noreply.github.com>
2023-12-13 11:40:31 -0800

    Update Events.jsx

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -13,7 +13,7 @@ export default function Events() {
       {events.length > 0 ? (
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
           {events?.map((event, index) => (
-            <a key={index} href={event?.link} className="" target="_blank">
+            <a key={index} href={event?.link} className="" target="_blank" rel="noopener">
               <img
                 className="hover:opacity-70 h-fit max-h-[304px] w-full object-cover object-center"
                 src={event.imgUrl}
